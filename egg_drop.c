37f869d6c383392d455999852eb2e518534a4def
Brian Stout <brianjonstout@gmail.com>
2017-01-20 18:22:08 -0500

    Binary search option.

--- egg_drop.c
+++ egg_drop.c
@@ -1,63 +1,98 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sysexits.h>
 #include <math.h>
 #include "egg.h"
 
 
-int worst_case(double numberOfFloors)
+int worst_case(double floorAmount)
 {
-    return (int)ceil((sqrt(1+8*numberOfFloors)-1)/2);
+    return (int)ceil((sqrt(1+8*floorAmount)-1)/2);
 }
-//Create list of eggs?
 
-void test_eggs(egg **nest, size_t number_of_eggs, size_t number_of_floors, size_t current_floor)
+void binary_egg_drop(size_t floorAmount)
 {
-    int egg;
-    int worstCase;
-    int decrement = 1;
-    if(number_of_eggs > 1)
-    {
-    worstCase = worst_case(number_of_floors);
-    egg = 0;
-    } else {
-        worstCase = 1;
-        egg = 1;
-        decrement = 0;
-    }
+    //TODO: Calculate jump amount with lastknownsafe and lastknowndangerous
+    //          instead of using a seperate double value
 
-    while(!egg_is_broken(nest[egg]))
+    egg *testEgg = lay_egg();
+    size_t dropNumber = 0;
+    size_t eggNumber = 1;
+    double jumpAmount = ceil(floorAmount/2);
+    size_t testFloor = jumpAmount;
+    size_t lastKnownSafe = 0;
+    size_t lastKnownDangerous = 0;
+
+    printf("Dropping egg#1 from floor:\n");
+    while(true)
     {
-        current_floor+=worstCase;
-        worstCase -= decrement;
-        egg_drop_from_floor(nest[egg], current_floor);
-        if(!egg_is_broken(nest[egg]))
+        jumpAmount = ceil(jumpAmount/2);
+        egg_drop_from_floor(testEgg, testFloor);
+        dropNumber++;
+
+        if(egg_is_broken(testEgg))
         {
-            printf("Floor %d; Safe!\n", current_floor);
+            printf("#%zd CRACK\n", testFloor);
+            lastKnownDangerous = testFloor;
+            destroy_egg(testEgg);
+            if(testFloor-lastKnownSafe == 1) { break; }
+
+            testEgg = lay_egg();
+            eggNumber += 1;
+
+            printf("Dropping egg#%zd from floor:\n", eggNumber);
+            testFloor = lastKnownSafe + jumpAmount;
         }
-        else{
-            --number_of_eggs;
-            printf("Floor %d: Crack!\n", current_floor);
+        else
+        {
+            printf("#%zd safe\n", testFloor);
+            if(lastKnownDangerous-testFloor == 1) { break; }
+            lastKnownSafe = testFloor;
+            testFloor += jumpAmount;
         }
+
     }
-    current_floor -= worstCase += decrement;
-    if(number_of_eggs != 0){
-    test_eggs(nest, number_of_eggs, number_of_floors, current_floor);
-    }
-    else{
-    printf("The last safe floor is %d\n", current_floor--); }
+    printf("The maximum safe floor is %zd, found after %zd drops\n", lastKnownSafe, dropNumber);
 }
 
 
-int main(void)
+void drop_eggs(size_t eggAmount, size_t floorAmount)
 {
-    size_t number_of_floors = 100;
-    size_t number_of_eggs = 2;
 
-    egg *nest[10];
-    nest[0] = lay_egg();
-    nest[1] = lay_egg();
-    
-    test_eggs(nest, number_of_eggs, number_of_floors, 0);
+}
 
 
-    printf("%d\n", worst_case(number_of_floors));
+int main( int argc, char *argv[] )
+{
+    size_t floorAmount;
+    size_t eggAmount;
+
+    //TODO: ADD ERROR HANDLING FOR ARGUMENTS
+    if (argc == 1)
+    {
+        floorAmount = 100;
+        eggAmount = 2;
+    }
+    else if (argc == 2)
+    {
+        floorAmount = atoi(argv[1]);
+        eggAmount = 2;        
+    }
+    else if (argc == 3)
+    {
+        floorAmount = atoi(argv[1]);
+        eggAmount = atoi(argv[2]);
+    }
+    else
+    {
+        printf("ERROR, USAGE: [./egg_drop] [<number of floors>] [<number of eggs>]\n");
+        return EX_USAGE;
+    }
+
+    if(eggAmount > log2(floorAmount)){
+        binary_egg_drop(floorAmount);
+    }
+
+
 }
